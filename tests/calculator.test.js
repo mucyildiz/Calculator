923afe5834a3d61dabbd5e01cc2d17be3e581cb7
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-04-23 15:09:49 -0400

    refactored tests for cleaner code and to make it easier to add tests

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,94 +1,67 @@
 const { evaluateExpression, getArrayOfElements, convertToPostfix, fixSingleElementsInParentheses, fixNegativeNumbers } = require('../scripts/calculator.js')
 
-describe("arithmetic tests", () => {
-	test("1 + 2 = 3", () => {
-		expect(evaluateExpression("1 + 2")).toBe(3);
-	});
-
-	test("4*5/2 = 10", () => {
-		expect(evaluateExpression("4*5/2")).toBe(10);
-	});
-
-	test("-5+-8--11*2 = 9", () => {
-		expect(evaluateExpression("-5+-8--11*2")).toBe(9);
-	});
-
-	test("-.32       /.5 = -0.64", () => {
-		expect(evaluateExpression("-.32       /.5")).toBe(-0.64);
-	});
-
-	test("(4-2)*3.5 = 7", () => {
-		expect(evaluateExpression("(4-2)*3.5")).toBe(7);
-	});
+describe("evaluateExpression tests", () => {
+	const testEvaluateExpression = (testInputStr, expectedOutputNum) => {
+		test(`${testInputStr} = ${expectedOutputNum}`, () => {
+			expect(evaluateExpression(testInputStr)).toBe(expectedOutputNum);
+		});
+	}
+	testEvaluateExpression("1 + 2 = 3", "1 + 2", 3);
+	testEvaluateExpression("4*5/2 = 10", "4*5/2", 10);
+	testEvaluateExpression("-5+-8--11*2 = 9", "-5+-8--11*2", 9);
+	testEvaluateExpression("-.32       /.5 = -0.64", "-.32       /.5", -0.64);
+	testEvaluateExpression("(4-2)*3.5 = 7", "(4-2)*3.5", 7);
 });
 
 describe("input validation", () => {
-	test("2+-+-4 gives Invalid Input", () => {
-		expect(() => {evaluateExpression("2+-+-4")}).toThrow("Invalid Input");
-	});
-
-	test("19 + cinnamon gives Invalid Input", () => {
-		expect(() => {evaluateExpression("19 + cinnamon")}).toThrow("Invalid Input");
-	});
-
-	test("(2+(3) gives Invalid Input", () => {
-		expect(() => {evaluateExpression("(2+(3)")}).toThrow("Invalid Input");
-	});
+	const testValidInput = (testInputStr) => {
+		test(`${testInputStr} gives Invalid Input`, () => {
+			expect(() => {evaluateExpression(testInputStr)}).toThrow("Invalid Input")
+		})
+	}
+	testValidInput("2+-+-4");
+	testValidInput("19 + cinnamon");
+	testValidInput("(2+(3)");
+	testValidInput("3.1415 + 78 +3.");
 });
 
-describe("getArrayOfElements", () => {
-	test("34 +7*8 gives ['34', '+', '7', '*', '8']", () => {
-		expect(getArrayOfElements("34 +7*8")).toStrictEqual(['34', '+', '7', '*', '8']);
-	});
-
-	test("24 + 7 * 80", () => {
-		expect(getArrayOfElements("24 + 7 * 80")).toStrictEqual(['24', '+', '7', '*', '80'])
-	})
-
-	test("3.1415 + 78 +3. gives error", () => {
-		expect(() => {getArrayOfElements("3.1415 + 78 +3.")}).toThrow("Invalid Input");
-	});
-
-	test("(35*7+3.512412)  + 4*72", () => {
-		expect(getArrayOfElements("(35*7+3.512412)  + 4*72")).toStrictEqual(['(', '35', '*', '7', '+', '3.512412', ')', '+', '4', '*', '72']);
-	});
+describe("getArrayOfElements tests", () => {
+	const testCorrectArrayReturned = (testInputStr, expectedOutputArray) => {
+		test(`${testInputStr} gives [${expectedOutputArray}]`, () => {
+			expect(getArrayOfElements(testInputStr)).toStrictEqual(expectedOutputArray);
+		})
+	}
+	testCorrectArrayReturned("34 +7*8", ['34', '+', '7', '*', '8']);
+	testCorrectArrayReturned("24 + 7 * 80", ['24', '+', '7', '*', '80']);
+	testCorrectArrayReturned("(35*7+3.512412)  + 4*72", ['(', '35', '*', '7', '+', '3.512412', ')', '+', '4', '*', '72'])
 });
 
-describe("convertToPostfix", () => {
-	test("4+5 gives ['4', '5', '+']", () => {
-		expect(convertToPostfix("4+5")).toStrictEqual(['4', '5', '+']);
-	});
+describe("convertToPostfix tests", () => {
+	const testCorrectPostfixNotation = (testInputStr, expectedOutputArray) => {
+		test(`${testInputStr} gives [${expectedOutputArray}]`, () => {
+			expect(convertToPostfix(testInputStr)).toStrictEqual(expectedOutputArray)
+		})
+	}
+	testCorrectPostfixNotation("4+5", ['4', '5', '+']);
+	testCorrectPostfixNotation("3.145 + 4 * 8", ['3.1415', '4', '8', '*', '+'])
+	testCorrectPostfixNotation("4(8)", ['4', '8', '*']);
+	testCorrectPostfixNotation("4*5/2", ['4', '5', '*', '2', '/'])
 
-	test("3.145 + 4 * 8", () => {
-		expect(convertToPostfix("3.1415+4*8")).toStrictEqual(['3.1415', '4', '8', '*', '+']);
-	});
+describe("fixing tests", () => {
+	//input should be the string representation of the equation, gets converted to array to be evaluated
+	const testFixSingleElementsInParentheses = (testInputStr, expectedOutputArray) => {
+		const tokenArray = getArrayOfElements(testInputStr);
+		expect(fixSingleElementsInParentheses(tokenArray)).toStrictEqual(expectedOutputArray)
+	}
+})
+	testFixSingleElementsInParentheses("4(6)2", ['4','*', '6', '*', '2']);
+	testFixSingleElementsInParentheses("5+4(3)", ['5','+', '4', '*', '3'])
 
-	test("4(8)", () => {
-		expect(convertToPostfix("4(8)")).toStrictEqual(['4','8','*']);
-	});
-
-	test("4*5/2", () => {
-		expect(convertToPostfix("4*5/2")).toStrictEqual(['4', '5', '*', '2', '/']);
-	})
-
-	test("4(6)2 evaluates to 4*6*2", () => {
-		const tokenArray = getArrayOfElements("4(6)(2)");
-		expect(fixSingleElementsInParentheses(tokenArray)).toStrictEqual(['4','*', '6', '*', '2']);
-	})
-
-	test("5+4(3) evaluates to 5 + 4 * 3", () => {
-		const tokenArray = getArrayOfElements("5+4(3)");
-		expect(fixSingleElementsInParentheses(tokenArray)).toStrictEqual(['5','+', '4', '*', '3']);
-	})
-
-	test("[5, -, -, 4] evaluates to [5, +, -4]", () => {
-		const tokenArray = ['5', '-', '-', '4'];
-		expect(fixNegativeNumbers(tokenArray)).toStrictEqual(['5', '-', '-4'])
-	})
-
-	test("[5, -, 4] evaluates to [5, +, -4]", () => {
-		const tokenArray = ['5', '-', '4'];
-		expect(fixNegativeNumbers(tokenArray)).toStrictEqual(['5', '+', '-4'])
-	})
-
+	const testFixNegativeNumbers = (testInputArray, expectedOutputArray) => {
+		test(`[${testInputArray}] evaluates to [${expectedOutputArray}]`, () => {
+			expect(fixNegativeNumbers(testInputArray)).toStrictEqual(expectedOutputArray);
+		})
+	}
+	testFixNegativeNumbers(['5', '-', '-', '4'], ['5', '-', '-4']);
+	testFixNegativeNumbers(['5', '-', '4'], ['5', '+', '-4']);
 })
