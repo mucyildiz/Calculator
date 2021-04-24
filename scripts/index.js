07cc5d9aaf12ee20e15fd94860cedd280f83878e
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-04-24 03:12:53 -0400

    added handler for =, added check for invalid input

--- scripts/index.js
+++ scripts/index.js
@@ -10,11 +10,36 @@ const handleClick = e => {
   if(value === 'Clear'){
     handleClear();
   }
+  else if(value === '='){
+    const result = evaluateExpression(equation.value);
+    equation.value = result;
+  }
   else{
     equation.value += value;
   }
 }
 
+const evaluateExpressionDOM = input => {
+  try {
+    const result = evaluateExpression(input);
+    return result;
+  }
+  catch (err) {
+    equation.value = '';
+    equation.placeholder = 'Invalid Input';
+  }
+} 
+
 for(button of buttons) {
   button.addEventListener('click', handleClick);
 }
+
+document.addEventListener("keydown", e => {
+  if (e.code === "Enter"){
+    evaluateExpressionDOM(equation.value);
+  }
+  // shouldn't have "Invalid Input" if we erase input after getting invalid input
+  if(equation.value.length === 1 && e.code === "Backspace"){
+    equation.placeholder = '';
+  }
+})
